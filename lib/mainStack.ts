c6d1660e4adb85b991adb7b1b6a62fbe6256c893
Cristian Magana <cmagana@email.com>
2022-12-19 18:46:35 -0800

    Testing lambda integration

--- lib/mainStack.ts
+++ lib/mainStack.ts
@@ -3,11 +3,40 @@ import { Construct } from "constructs";
 import { ApplicationProps } from "../bin/microservice_b";
 import { Stack } from "aws-cdk-lib";
 import { Queue } from "aws-cdk-lib/aws-sqs";
+import { ApiGatewayFullProps, createApiGateway, createNodeJsLambda } from "@digitalxtian/aws-cdk-pipeline";
 
 export class MainStack extends Stack {
   constructor(scope: Construct, id: string, props: ApplicationProps) {
     super(scope, id, props);
 
-    new Queue(this, "Queue");
+    const getLambda = createNodeJsLambda(this, `${id}-get-lambda`, 
+      'lambda/coolMicroservice/src/getHandler/index.ts', 
+      {
+        functionName: 'getLambda',
+        handler: 'getHandler',
+
+      });
+
+      const postLambda = createNodeJsLambda(this,`${id}-post-lambda`, 
+      'lambda/coolMicroservice/src/postHandler/index.ts', {
+        functionName: 'postLambda',
+        handler: 'postHandler',
+      });
+
+      const gatewayProps: ApiGatewayFullProps = {
+        resources: [
+          {
+            resourceName: 'coolResource',
+            methodGet: {
+              lambda: getLambda
+            },
+            methodPost: {
+              lambda: postLambda
+            },
+          }
+        ]
+      }
+
+      createApiGateway(this, `${id}-apigw`, gatewayProps);
   }
 }
