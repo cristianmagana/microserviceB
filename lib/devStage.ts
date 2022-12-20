fbc6552c8b8b3c001c1f7505934a9389cd3b358d
Cristian Magana <cmagana@email.com>
2022-12-19 18:49:53 -0800

    Testing lambda integration

--- lib/devStage.ts
+++ lib/devStage.ts
@@ -2,8 +2,6 @@ import { Stage } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ApplicationProps } from "../bin/microservice_b";
 import { MainStack } from "./mainStack";
-import { CoolMicroservice } from "./coolMicroservice";
-
 export class DevStage extends Stage {
   constructor(scope: Construct, id: string, props: ApplicationProps) {
     super(scope, id, props);
@@ -13,7 +11,5 @@ export class DevStage extends Stage {
       `main-stack-${props.env.name}-${props.env.region}`,
       props
     );
-
-    new CoolMicroservice(this, `cool-stack-${props.env.name}-${props.env.region}`, props);
   }
 }
--- lib/mainStack.ts
+++ lib/mainStack.ts
@@ -1,8 +1,6 @@
-import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ApplicationProps } from "../bin/microservice_b";
 import { Stack } from "aws-cdk-lib";
-import { Queue } from "aws-cdk-lib/aws-sqs";
 import { ApiGatewayFullProps, createApiGateway, createNodeJsLambda } from "@digitalxtian/aws-cdk-pipeline";
 
 export class MainStack extends Stack {
