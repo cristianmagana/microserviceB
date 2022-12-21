7c0bb64a785e62d7f2c92bef714cc240b1b1016e
Cristian Magana <cmagana@email.com>
2022-12-20 19:37:00 -0800

    v1.5.2

--- bin/microservice_b.ts
+++ bin/microservice_b.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { MicroserviceBStack } from "../lib/microservice_b-stack";
+import { MicroserviceBStack } from "../lib/microservice-stack";
 
 const app = new cdk.App();
 
---
+++ lib/microservice-stack.ts
@@ -0,0 +1,111 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { createPipeline } from "@digitalxtian/aws-cdk-pipeline";
+import { ApplicationProps } from "../bin/microservice_b";
+import { Stack } from "aws-cdk-lib";
+import { DevStage } from "./devStage";
+import { UatStage } from "./uatStage";
+import { ProdStage } from "./prodStage";
+import { BuildSpec, ComputeType, LinuxBuildImage, ReportGroup } from "aws-cdk-lib/aws-codebuild";
+import { CodeBuildStep, CodePipelineSource } from "aws-cdk-lib/pipelines";
+import { CodeBuildAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+export class MicroserviceBStack extends Stack {
+  constructor(scope: Construct, id: string, props: ApplicationProps) {
+    super(scope, id, props);
+
+    const { pipeline, devEnvironment, uatEnvironment, prodEnvironment } =
+      createPipeline(this, "microserviceB-pipeline", {
+        name: `${props.name}-pipeline`,
+        ghRepository: "cristianmagana/microserviceB",
+        codeArtifactDomain: props.codeArtifactDomain,
+        codeArtifactRepo: props.codeArtifactRepo,
+        loadTestName: "loadtest.yaml",
+        activateLambdaLoadTestings: true,
+        env: {
+          name: props.env.name,
+          account: props.env.account,
+          region: props.env.region,
+          branch: "master",
+        },
+      });
+
+    const devEnvs = props.environments.filter(
+      (environments) => environments.env.name === "dev"
+    );
+    for (const devEnv of devEnvs)
+      devEnvironment.addStage(
+        new DevStage(this, `dev-stage-${devEnv.env.region}`, { ...devEnv })
+      );
+
+    const uatEnvs = props.environments.filter(
+      (environments) => environments.env.name === "uat"
+    );
+    for (const uatEnv of uatEnvs)
+      uatEnvironment.addStage(
+        new UatStage(this, `uat-stage-${uatEnv.env.region}`, { ...uatEnv })
+      );
+
+    const prodEnvs = props.environments.filter(
+      (environments) => environments.env.name === "prod"
+    );
+    for (const prodEnv of prodEnvs)
+      prodEnvironment.addStage(
+        new ProdStage(this, `prod-stage-${prodEnv.env.region}`, { ...prodEnv })
+      );
+
+    const loadTestReportGroup = new ReportGroup(scope, "LoadTestReportGroup", {});
+    const loadBuildTest = new CodeBuildStep(`${props.name}-load-testing-step`, {
+      input: CodePipelineSource.connection(`${props.ghRepository}`, 'master', {
+          connectionArn: props.codeStarConnectionArn,
+      }),
+      installCommands: ['npm install -g artillery@latest'],
+      commands: ['npm install -g npm@latest', 'npm ci', `artillery run test/${props.loadTestName}`],
+      rolePolicyStatements: [
+        new PolicyStatement({
+            actions: ['sts:AssumeRole'],
+            resources: ['*'],
+            conditions: {
+                StringEquals: {
+                    'iam:ResourceTag/aws-cdk:bootstrap-role': 'lookup',
+                },
+            },
+        }),
+        new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                'codeartifact:GetAuthorizationToken',
+                'codeartifact:ReadFromRepository',
+                'codeartifact:GetRepositoryEndpoint',
+                'codeartifact:PublishPackageVersion',
+            ],
+            resources: ['*'],
+        }),
+        new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['sts:GetServiceBearerToken'],
+            resources: ['*'],
+        }),
+    ],
+      buildEnvironment: {
+        computeType: ComputeType.SMALL,
+        buildImage: LinuxBuildImage.STANDARD_6_0,
+        privileged: true,
+    },
+      primaryOutputDirectory: 'cdk.out',
+      partialBuildSpec: BuildSpec.fromObject({
+          version: '0.2',
+          reports: {
+              [loadTestReportGroup.reportGroupArn]: {
+                  files: ['reports/*'],
+              },
+          },
+      }),
+  });
+
+    devEnvironment.addPost(loadBuildTest);
+
+    pipeline.buildPipeline();
+  }
+}
